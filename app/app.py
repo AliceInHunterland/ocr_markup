ae0afd5b6c144fa64d2e4d2f55a4b06e4476617e
OlegJakushkin <oleg.jakushkin@gmail.com>
2021-09-28 15:09:45 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 #/usr/bin/python
+import os
 from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 from tesserocr import PyTessBaseAPI, RIL
@@ -9,10 +10,9 @@ app = FastAPI()
 
 
 @app.post("/get_ocr")
-async def image(image: UploadFile = File(...), json : str = "{'name':[x,y,w,h]}"):
-    print(image.file, json)
+async def image(image: UploadFile = File(...), json_boxes : str = """[{"name": 		{"x":0,"y":0,"w":300,"h":300} } ]"""):
+    print(image.file, json_boxes)
     
-    # print('../'+os.path.isdir(os.getcwd()+"images"),"*************")
     try:
         os.mkdir("images")
         print(os.getcwd())
@@ -23,22 +23,24 @@ async def image(image: UploadFile = File(...), json : str = "{'name':[x,y,w,h]}"
         f.write(image.file.read())
         f.close()
     
-    
+    results = {}
     image = Image.open(file_name) #TODO: remove store to FS!!!
     with PyTessBaseAPI() as api:
         api.SetImage(image)
-        boxes = api.GetComponentImages(RIL.TEXTLINE, True)
-        print('Found {} textline image components.'.format(len(boxes)))
-        for i, (im, box, _, _) in enumerate(boxes):
+        #boxes = api.GetComponentImages(RIL.TEXTLINE, True)
+        #print('Found {} textline image components.'.format(len(boxes)))
+        boxes =json.loads(json_boxes) 
+        print(boxes)
+        for key, box in  boxes.items():
             # im is a PIL image object
             # box is a dict with x, y, w and h keys
             api.SetRectangle(box['x'], box['y'], box['w'], box['h'])
             ocrResult = api.GetUTF8Text()
             conf = api.MeanTextConf()
+            print(key)
+            
             print(u"Box[{0}]: x={x}, y={y}, w={w}, h={h}, "
                   "confidence: {1}, text: {2}".format(i, conf, ocrResult, **box))
-        
-        
-    file = jsonable_encoder({"imagePath":file_name})
-    new_image = await add_image(file)
-    return {"filename": new_image}
+            results[key] = ocrResult
+
+    return results
